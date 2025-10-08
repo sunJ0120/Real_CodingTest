a1d27c7be774e93250265d153239ad28cfb4814e
sunJ0120 <sspure1214@gmail.com>
2025-10-08 13:32:50 +0900

    fix : set.stream().sorted를 통해서 바로 set을 정렬해서 내보내기

--- 1_배열/두개 뽑아서 더하기.java
+++ 1_배열/두개 뽑아서 더하기.java
@@ -2,28 +2,14 @@ import java.util.*;
 
 class 두개_뽑아서_더하기 {
     public int[] solution(int[] numbers) {
-        Set<Integer> set = new TreeSet<Integer>(); //컴파일 비용을 줄이기 위함이다.
+        Set<Integer> set = new HashSet<Integer>(); //컴파일 비용을 줄이기 위함이다.
 
         for(int i = 0; i<numbers.length-1; i++){
             for(int j = i+1; j<numbers.length; j++){
                 set.add(numbers[i] + numbers[j]);
             }
         }
-
-        // set to list
-        List<Integer> list = new ArrayList<Integer>();
-
-        for(int n : set){
-            list.add(n);
-        }
-
-        // 정렬
-        Collections.sort(list);
-
-        // Array to list..이건 외우기
-        int[] answer = list.stream().mapToInt(Integer::intValue).toArray();
-
-        return answer;
+        return set.stream().sorted().mapToInt(Integer::intValue).toArray();
     }
 
     public static void main(String[] args) {
