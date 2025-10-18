7979331612f9f3cc868ac501c039da5c4bf475ba
sunJ0120 <sspure1214@gmail.com>
2025-10-18 19:07:48 +0900

    fix: 컬렉션 대신 배열 쓰게 변경 - 더 명확함
    - 컬렉션은 동적인 조절이 필요할때만 사용한다.

--- 2_스택/크레인_인형뽑기_게임/복습/1차/크레인_인형_뽑기.java
+++ 2_스택/크레인_인형뽑기_게임/복습/1차/크레인_인형_뽑기.java
@@ -1,6 +1,4 @@
 import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.List;
 
 public class 크레인_인형_뽑기 {
     public static void main(String[] args) {
@@ -13,31 +11,32 @@ public class 크레인_인형_뽑기 {
 
     public int solution(int[][] board, int[] moves) {
         int answer = 0;
+
+        int rows = board.length; //행
+        int cols = board[0].length; //열
+
         // 1. 열 하나하나를 스택으로 만든다.
-        List<ArrayDeque<Integer>> crain = new ArrayList<ArrayDeque<Integer>>();
+        ArrayDeque[] crain = new ArrayDeque[cols];
         ArrayDeque<Integer> bucket = new ArrayDeque<Integer>();
 
-        for (int m = 0; m < board[0].length; m++) {
+        for (int m = 0; m < cols; m++) {
             ArrayDeque<Integer> stack = new ArrayDeque<Integer>();
-            for (int n = board.length - 1; n >= 0; n--) {
+            for (int n = rows - 1; n >= 0; n--) { //행 일관되게 하기 위해 -1 여기에 붙이기
                 if (board[n][m] == 0) {
                     break;
                 }
                 stack.push(board[n][m]);
             }
-            crain.add(stack);
+            crain[m] = stack;
         }
         // 2. 위치에 맞게 pop으로 가져온다. (거꾸로)
         for (int move : moves) {
             // 3. 위치에 따라 (-1해서 인덱스 맞추기) 가져오면서 맞춰본다.
-            ArrayDeque<Integer> stack = crain.get(move - 1);
-            // System.out.println(stack.toString());
+            ArrayDeque<Integer> stack = crain[move - 1];
             if (stack.isEmpty()) {
                 continue;
             }
             int doll = stack.pop();
-            // System.out.println("doll : " + doll);
-            // System.out.println("bucket.peek() : " + bucket.peek());
             if (!bucket.isEmpty() && bucket.peek() == doll) {
                 // 4. 터지면 두개씩 증가
                 answer += 2;
