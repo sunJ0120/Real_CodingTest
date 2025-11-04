0a52a5b528ad47baa8991d356556f4902865b26a
sunJ0120 <sspure1214@gmail.com>
2025-11-04 23:08:06 +0900

    solved: 백준 7576. 토마토 풀이 완료
    - 모든 토마토가 동시에 익기 시작하기에 시작점이 여러개라는게 포인트이다.
    - 또한, 가장 깊은 depth를 구하는게 익는 날짜이다.

--- 4_그래프/토마토/토마토.java
+++ 4_그래프/토마토/토마토.java
@@ -1,76 +1,89 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.lang.*;
+import java.util.*;
 
-public class 토마토 {
-    public static final int[] posX = {0, 0, -1, 1};
-    public static final int[] posY = {-1, 1, 0, 0};
-    public static int n;
+public class 토마토{
     public static int m;
-    public static int ans = 0;
-    public static int days = 0;
+    public static int n;
+    public static final int[] posX = {0,0,-1,1};
+    public static final int[] posY = {-1,1,0,0};
 
+    public boolean isTrueInd(int x, int y){
+        if((0<=x && x<m) && (0<=y && y<n)){
+            return true;
+        }
+        return false;
+    }
+
+    public void addQueue(ArrayDeque<int[]> queue, boolean[][] isVisit, int x, int y, int depth){
+        int[] pos = new int[3];
+        pos[0] = x;
+        pos[1] = y;
+        pos[2] = depth;
+        queue.addLast(pos);
+        isVisit[y][x] = true;
+    }
+
+    public int bfs(ArrayDeque<int[]> queue, boolean[][] isVisit, String[][] tomatos){
+        int depth = 0;
+
+        // 3. 큐가 비었다면 더 갈곳 없는거라 끝낸다.
+        while(!queue.isEmpty()){
+            int[] top = queue.pollFirst();
+            int topDepth = top[2];
+            depth = Math.max(topDepth,depth);  // 밖에서 depth 갱신
+
+            // 2. 큐에 있는거 4방향을 체크해서 올린다.
+            for(int i = 0; i<4; i++){
+                int nextX = top[0] + posX[i];
+                int nextY = top[1] + posY[i];
+
+                if(isTrueInd(nextX, nextY)
+                        && !isVisit[nextY][nextX]
+                        && !("-1".equals(tomatos[nextY][nextX]))){
+                    addQueue(queue, isVisit, nextX, nextY,topDepth+1);
+                    tomatos[nextY][nextX] = "1";    // 🍅 익음
+                }
+            }
+        }
+        return depth;
+    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        m = Integer.parseInt(st.nextToken());    // 가로
-        n = Integer.parseInt(st.nextToken());    // 세로
+        m = Integer.parseInt(st.nextToken());   //가로
+        n = Integer.parseInt(st.nextToken());   //세로
 
         String[][] tomatos = new String[n][m];
         boolean[][] isVisit = new boolean[n][m];
+        ArrayDeque<int[]> queue = new ArrayDeque<int[]>();    // bfs
 
-        for (int i = 0; i < n; i++) {
+        for(int i = 0; i<n; i++){
             tomatos[i] = br.readLine().split(" ");
         }
 
         토마토 sol = new 토마토();
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                // 1. 방문한적 없고 1인 곳을 찾는다.
-                if (!isVisit[i][j] && "1".equals(tomatos[i][j])) {
-                    isVisit[i][j] = true;
-                    sol.dfs(tomatos, isVisit, j, i, 0);
-                    ans += days;
-                    days = 0;
+        for(int i = 0; i<n; i++){
+            for(int j = 0; j<m; j++){
+                // 1. isVisit 하지 않고, 1이면 큐에 올린다.
+                if(!isVisit[i][j] && "1".equals(tomatos[i][j])){
+                    sol.addQueue(queue, isVisit, j, i, 0);
                 }
             }
         }
 
-        // 4. 다 빠져 나오고 검사해서 0이 있으면 -1로 return 한다.
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if ("0".equals(tomatos[i][j])) {
+        int ans = sol.bfs(queue, isVisit, tomatos);
+
+        for(int i = 0; i<n; i++){
+            for(int j = 0; j<m; j++){
+                // 4. 0이 있으면 return -1, 아니면 ans 내보낸다.
+                if("0".equals(tomatos[i][j])){
                     System.out.println(-1);
                     return;
                 }
             }
         }
         System.out.println(ans);
-    }
-
-    public boolean isTrueInd(int x, int y) {
-        if ((0 <= x && x < m) && (0 <= y && y < n)) {
-            return true;
-        }
-        return false;
-    }
-
-    public void dfs(String[][] tomatos, boolean[][] isVisit, int x, int y, int day) {
-        // 2. 4방향 탐색으로 재귀 돈다.
-        for (int i = 0; i < 4; i++) {
-            int nextX = x + posX[i];
-            int nextY = y + posY[i];
-            if (isTrueInd(nextX, nextY)
-                    && !isVisit[nextY][nextX]
-                    && !("-1".equals(tomatos[nextY][nextX]))) {
-
-                isVisit[nextY][nextX] = true;
-                tomatos[nextY][nextX] = "1";
-
-                dfs(tomatos, isVisit, nextX, nextY, day + 1);
-            }
-        }
     }
 }
