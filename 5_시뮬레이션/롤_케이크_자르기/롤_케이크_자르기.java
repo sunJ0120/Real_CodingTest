b4850d2cabcff8d7f9e89cc28d8f85112348adb1
sunJ0120 <sspure1214@gmail.com>
2025-11-12 22:55:13 +0900

    solved: [시뮬레이션] 롤 케이크 자르기 문제 풀이 완료

--- 5_시뮬레이션/롤_케이크_자르기/롤_케이크_자르기.java
+++ 5_시뮬레이션/롤_케이크_자르기/롤_케이크_자르기.java
@@ -2,51 +2,38 @@ import java.lang.*;
 import java.util.*;
 
 class 롤_케이크_자르기 {
-    public static int ans = 0;
-    public static boolean[] isVisit;
+    public void moveTopping(HashMap<Integer, Integer> left, HashSet<Integer> right, int topping){
+        right.add(topping);
+        left.put(topping, left.get(topping) - 1);
 
-    public HashSet<Integer> makeSet(int start, int end, int[] topping){
-        HashSet<Integer> set = new HashSet<Integer>();
-        for(int i = start; i<end; i++){
-            set.add(topping[i]);
+        if(left.get(topping) == 0){
+            left.remove(topping);
         }
-        return set;
-    }
-
-    public void cutRollCake(int mid, int[] topping){
-        // 5. end == -1이거나 end == n-1이면 return으로 종료
-        if(mid == 0 || mid == topping.length){
-            return;
-        }
-
-        // 이미 방문 했으면 종료
-        if(isVisit[mid]){
-            return;
-        }
-
-        isVisit[mid] = true;    // 방문 체크
-
-        // 2. end로 왼쪽(미포함), 오른쪽 나눈다.
-        Set<Integer> setA = makeSet(0, mid, topping);
-        Set<Integer> setB = makeSet(mid, topping.length, topping);
-
-        // 3. 왼쪽 set == 오른쪽 set이면 통과
-        if(setA.size() == setB.size()){
-            ans++;
-        }
-        // 4. end-1한거랑 end+1 한거를 mid로 두고 재귀한다
-        cutRollCake(mid+1, topping);
-        cutRollCake(mid-1, topping);
     }
 
     public int solution(int[] topping) {
-        // 1. 가운데 기준으로 end 값을 구한다.
-        int mid = topping.length / 2;
-        // 방문 체크를 해야 같은 곳 방문을 안한다.
-        isVisit = new boolean[topping.length];
-        cutRollCake(mid, topping);
+        int answer = 0;
 
-        // 6. ans return 한다.
-        return ans;
+        HashMap<Integer, Integer> left = new HashMap<Integer, Integer>();
+        for(int top : topping){
+            left.put(top, left.getOrDefault(top,0) + 1);
+        }
+
+        HashSet<Integer> right = new HashSet<Integer>();
+
+        for(int top : topping){
+            moveTopping(left, right, top);
+            if(right.size() == left.size()){
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        롤_케이크_자르기 sol = new 롤_케이크_자르기();
+        int[] topping = {1, 2, 1, 3, 1, 4, 1, 2};
+        System.out.println(sol.solution(topping));
     }
 }
