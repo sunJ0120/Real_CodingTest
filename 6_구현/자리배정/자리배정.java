3cbdf62ff42cd66fcc30846ce5dba7fb8ea92f8e
sunJ0120 <sspure1214@gmail.com>
2025-11-17 23:13:30 +0900

    refactor: [구현] 백준 10157. 자리배정 풀이 변경
    1. 나선형 배열의 경우, 방문을 체크하면 손쉽게 풀 수 있다.
    2. 방문 했거나, 범위에서 넘치는 경우 방향을 순서대로 바꿔서 헤드를 회전하는 방식으로 풀면 된다.

--- 6_구현/자리배정/자리배정.java
+++ 6_구현/자리배정/자리배정.java
@@ -5,19 +5,10 @@ import java.io.*;
 public class 자리배정{
     public static final int[] posX = {0,1,0,-1}; // 위 오른쪽 아래 왼쪽
     public static final int[] posY = {1,0,-1,0};
-    public static int ans;
-    public static int x;
-    public static int y;
 
-    public static boolean pos(int dir, int gallery, int end){
-        for(int i = 0; i<end; i++){
-            x += posX[dir];
-            y += posY[dir];
-            ans++;
-
-            if(ans == gallery){
-                return true;
-            }
+    public static boolean isTruInd(int x, int y, int c, int r) {
+        if((0<=x && x<c) && (0<=y && y<r)){
+            return true;
         }
         return false;
     }
@@ -25,39 +16,33 @@ public class 자리배정{
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int c = Integer.parseInt(st.nextToken());
-        int r = Integer.parseInt(st.nextToken());
-        int gallery = Integer.parseInt(br.readLine());
+        int c = Integer.parseInt(st.nextToken());   // x
+        int r = Integer.parseInt(st.nextToken());   // y
+        int gallery = Integer.parseInt(br.readLine());   // 관객
 
         if(gallery > c*r){
             System.out.println(0);
             return;
         }
 
-        ans = 0;
-        x = 0;
-        y = -1;
+        int x = 0;
+        int y = 0;
+        int dir = 0;
 
-        while(true){
-            if(pos(0, gallery, r)){
-                break;
-            }
-            r--;
+        boolean[][] isVisit = new boolean[r][c];
 
-            if(pos(1, gallery, c-1)){
-                break;
-            }
-            c--;
+        for(int i = 1; i<gallery; i++){
+            isVisit[y][x] = true;    // 여기까지가 i이고, 이후는 다음 좌표를 계산하는 것이라 <gallery로 표현
+            int nextX = x + posX[dir];
+            int nextY = y + posY[dir];
 
-            if(pos(2, gallery, r)){
-                break;
+            // 방문한적 있거나 벗어날 경우, 순서상 next를 갖고 검사해야 한다.
+            if(!isTruInd(nextX, nextY, c, r) || isVisit[nextY][nextX]){
+                dir = (dir+1) % 4;   // 방향 변경
             }
-            r--;
 
-            if(pos(3, gallery, c-1)){
-                break;
-            }
-            c--;
+            x += posX[dir];
+            y += posY[dir];
         }
         System.out.println((x+1) + " " + (y+1));
     }
