06e1041357914e836bb3fb70910cdc1571443c4e
sunJ0120 <sspure1214@gmail.com>
2025-11-20 00:20:43 +0900

    refactor: [구현] 백준 2304. 창고 다각형 큐 없이 풀기 완료
    - 그러나 아직, while 로직이 쓸데없이 복잡해서 최적화가 덜 된 느낌이다..

--- 6_구현/창고_다각형/창고_다각형.java
+++ 6_구현/창고_다각형/창고_다각형.java
@@ -2,80 +2,65 @@ import java.lang.*;
 import java.util.*;
 import java.io.*;
 
-public class 창고_다각형{
-    public static int areaCalcul(ArrayDeque<int[]> deque){
-        int answer = 0;
-        while(!deque.isEmpty()){
-            int[] a = deque.pollFirst();
-            if(!deque.isEmpty()){
-                int[] top = deque.peekFirst();
-                answer += Math.abs(top[0] - a[0]) * a[1];
-            }
-        }
-        return answer;
-    }
-
+public class 창고_다각형 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int cnt = Integer.parseInt(br.readLine());
-        StringTokenizer st;
-
-        int[][] pillar = new int[cnt][2];
-        ArrayDeque<int[]> deque = new ArrayDeque<int[]>();
-
-        int answer = 0;
+        int num = Integer.parseInt(br.readLine());
+        int[][] sticks = new int[num][2];
         int maxHeight = 0;
-        int maxInd = 0;    // 1. 가장 긴거 인덱스 구한다.
+        int maxInd = 0;
+        StringTokenizer st;
+        int ans = 0;
 
-        for(int i = 0; i<cnt; i++){
+        // 1. list에 넣기, 최대 높이 찾기
+        for(int i = 0; i<num; i++){
             st = new StringTokenizer(br.readLine());
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
+            sticks[i] = new int[]{x,y};
 
-            maxHeight = Math.max(maxHeight,y);
-
-            pillar[i] = new int[]{x,y};
+            maxHeight = Math.max(y, maxHeight);
         }
 
-        // x 기준으로 정렬하기
-        Arrays.sort(pillar, (a,b) -> Integer.compare(a[0], b[0]));
+        //2. list x로 정렬...이거 꼭 외우기
+        Arrays.sort(sticks, (a,b) -> Integer.compare(a[0], b[0]));
 
-        // maxInd 찾기
-        for(int i = 0; i<cnt; i++){
-            if(pillar[i][1] == maxHeight){
+        //3. 최대 인덱스를 구한다.
+        for(int i = 0; i<num; i++){
+            if(sticks[i][1] == maxHeight){
                 maxInd = i;
                 break;
             }
         }
 
-        // 2. 왼쪽 넓이 구하기
-        // 2-1. y가 증가추세면 올린다.
-        deque.addLast(pillar[0]);
-        for(int i = 1; i<=maxInd; i++){
-            int[] top = deque.peekLast();
-            if(top[1] <= pillar[i][1]){
-                deque.addLast(pillar[i]);
+        // 4. 왼쪽 구하기, 증가하면 x 구한다.
+        int start = 0;
+        int end = 1;
+        while(end <= maxInd){
+            if(sticks[start][1] <= sticks[end][1]){
+                ans += (Math.abs(sticks[end][0]-sticks[start][0]) * sticks[start][1]);
+                start = end; // start가 end 자리로 가서 자리 바꿔야 한다.
+                end++;
+                continue;
             }
+            end++;
         }
 
-        // 2-2. (top x - 뺀거 x) * 뺀거 y로 왼쪽 넓이 계산
-        answer += areaCalcul(deque);
-
-        // 3. 오른쪽 넓이 구하기
-        // 3-1. 끝에서부터, y가 증가추세면 올린다.
-        deque.addLast(pillar[cnt-1]);
-        for(int i = cnt-2; i>=maxInd; i--){
-            int[] top = deque.peekLast();
-            if(top[1] <= pillar[i][1]){
-                deque.addLast(pillar[i]);
+        // 5. 오른쪽 구하기, 증가하면 x 구한다.
+        start = num-1;
+        end = num-2;
+        while(end >= maxInd){
+            if(sticks[start][1] <= sticks[end][1]){
+                ans += (Math.abs(sticks[end][0]-sticks[start][0]) * sticks[start][1]);
+                start = end; // start가 end 자리로 가서 자리 바꿔야 한다.
+                end--;
+                continue;
             }
+            end--;
         }
+        // 6. 긴거 너비 더하기
+        ans += sticks[maxInd][1];
 
-        // 3-2. (top x - 뺀거 x) * 뺀거 y로 왼쪽 넓이 계산
-        answer += areaCalcul(deque);
-
-        answer += pillar[maxInd][1];
-
-        System.out.println(answer);
+        System.out.println(ans);
     }
 }
